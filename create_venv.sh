0e336b510037645f0a7f19ae65f88e4a6e5f1ba9
Paul Kronenwetter <kronenpj@gmail.com>
2023-12-15 10:24:37 -0500

    Rename venv directrory

--- create_venv.sh
+++ create_venv.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -x /usr/bin/python3.9 ];then
-  PY=python3.9
+if [ -x /usr/bin/python3.10 ];then
+  PY=python3.10
 else
   PY=python3
 fi
 
-/usr/bin/${PY} -m venv venv --clear --prompt VEdbcheck
-source venv/bin/activate
+/usr/bin/${PY} -m venv .venv --clear --prompt VEdbcheck
+source .venv/bin/activate
 #pip install poetry
 pip install -r requirements.txt -r requirements-test.txt
 #poetry install --no-root
